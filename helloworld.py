b9e4d5287c4182fecafd96f17cd8de2c1254a5c1
Oliver Frolovs <olliefr@users.noreply.github.com>
2020-09-03 03:44:07 +0100

    Init from Real Python template with amendments
    The template files from "Real Python" had been
    updated with my name and email address.

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# helloworld.py
+__version__ = '0.1.0'
+
+import re
+import requests
+
+URL = 'https://en.wikipedia.org/wiki/"Hello,_World!"_program'
+
+
+def do_hello():
+    result = requests.get(URL)
+    print(re.findall('<title>(.*?)</title>', result.text)[0])
+
+
+if __name__ == '__main__':
+    do_hello()
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os, sys, re
+
+# get version info from module without importing it
+version_re = re.compile("""__version__[\s]*=[\s]*['|"](.*)['|"]""")
+
+with open('helloworld.py') as f:
+    content = f.read()
+    match = version_re.search(content)
+    version = match.group(1)
+
+
+readme = os.path.join(os.path.dirname(__file__), 'README.md')
+long_description = open(readme).read()
+
+
+SETUP_ARGS = dict(
+    name='helloworld',
+    version=version,
+    description=('Grabs the "Hello World" Wikipedia page and prints its title'),
+    long_description=long_description,
+    url='https://github.com/olliefr/helloworld',
+    author='Oliver Frolovs',
+    author_email='one@cunty.dev',
+    license='MIT',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+    ],
+    py_modules = ['helloworld',],
+    install_requires = [
+        'requests>=2.22',
+    ],
+)
+
+if __name__ == '__main__':
+    from setuptools import setup, find_packages
+
+    SETUP_ARGS['packages'] = find_packages()
+    setup(**SETUP_ARGS)
